eeb7e134954f4c95d9295b914a3a639641824f0a
刘文龙 <1@gitor.org>
2018-03-09 23:53:46 +0800

    测试接口

--- src/main/java/api/javazs/com/myapi/apidoc/controller/Index/Test.java
+++
@@ -1,1 +0,0 @@
-package api.javazs.com.myapi.apidoc.controller.Index;/** * @author : liuwenlong * @date : 2018/3/8 17:29 */public class Test extends Thread implements Runnable{    public void run() {        System.out.println("....");    }    public static void main(String[] args) {        Thread thread = new Thread(new Test());        thread.start();    }}
